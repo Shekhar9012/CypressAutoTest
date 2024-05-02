7f98c02aa7a8185a7ec6b4b1aa77de4e84d7b45d
Shekhar9012 <chauhan.shekhar2015@gmail.com>
2024-05-02 23:30:34 +0530

    commit for fail test cases

--- cypress/e2e/checkboxes.cy.js
+++ cypress/e2e/checkboxes.cy.js
@@ -2,7 +2,7 @@ describe('Checkbox functionality', () => {
     it('Checkbox demo', () => {
         cy.visit('https://www.yatra.com/', {headers: {"Accept-Encoding": "gzip, deflate"}})
         cy.get('a[title="Non Stop Flights"]').click()
-        cy.wait(500)
+        cy.wait(1000)
         cy.get('#seniorcitizen_offer > .custom-check').click() //selecting particular checkbox
         //cy.get('[type="checkbox"]').click({ multiple: true }) // Selecting all checkboxes
 
